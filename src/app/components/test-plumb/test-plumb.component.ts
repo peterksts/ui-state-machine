4d4d4113bc04b08dc8dc3afdb684d61efe7637ee
peterksts <peterk@silvertreesystems.com>
2018-07-03 19:42:45 +0300

    add move item

--- src/app/components/test-plumb/test-plumb.component.ts
+++ src/app/components/test-plumb/test-plumb.component.ts
@@ -15,7 +15,7 @@ export class TestPlumbComponent implements OnInit {
   ngOnInit() {
     this.jsPlumbInstance = jsPlumb.getInstance();
     this.jsPlumbInstance.setContainer('diagramContainer');
-    //
+    // add endpoints
     this.jsPlumbInstance.addEndpoint('item_left',
       {
         anchor: 'Right',
@@ -87,5 +87,63 @@ export class TestPlumbComponent implements OnInit {
         connectorStyle: {stroke: 'green', strokeWidth: 1},
         connectorHoverStyle: {strokeWidth: 2}
       });
+    //
+    this.addListener();
+  }
+
+  private addListener(): void {
+    const main = this;
+    const divAll = document.getElementById('diagramContainer').
+      querySelectorAll('div');
+    // set EventListener
+    for (let i = 0; i < divAll.length; i++) {
+      const div = divAll.item(i);
+      // mouse down
+      div.addEventListener('mousedown', function (e) {
+        if (this.getAttribute('name') !== 'item_box') {
+          return;
+        }
+        //
+        move_div = this;
+        mouse_start_x = e.clientX;
+        mouse_start_y = e.clientY;
+        // mouse move
+        div.addEventListener('mousemove', main.moveItem);
+      });
+      // mouse up
+      div.addEventListener('mouseup', function () {
+        div.removeEventListener('mousemove', main.moveItem);
+      });
+    }
+  }
+
+  private moveItem(even): void {
+    const start_position_x = parseInt(move_div.style.left.slice(0, move_div.style.left.length - 2), null);
+    const start_position_y = parseInt(move_div.style.top.slice(0, move_div.style.top.length - 2), null);
+    let new_position_x = start_position_x;
+    let new_position_y = start_position_y;
+    // set new x item
+    if (even.clientX > mouse_start_x) {
+      new_position_x = start_position_x + (even.clientX - mouse_start_x);
+    }
+    if (even.clientX < mouse_start_x) {
+      new_position_x = start_position_x - (mouse_start_x - even.clientX);
+    }
+    // set new y item
+    if (even.clientY > mouse_start_y) {
+      new_position_y = start_position_y + (even.clientY - mouse_start_y);
+    }
+    if (even.clientY < mouse_start_y) {
+      new_position_y = start_position_y - (mouse_start_y - even.clientY);
+    }
+    // set position item
+    mouse_start_x = even.clientX;
+    mouse_start_y = even.clientY;
+    move_div.style.left = new_position_x + 'px';
+    move_div.style.top = new_position_y + 'px';
   }
 }
+
+let mouse_start_x;
+let mouse_start_y;
+let move_div;
