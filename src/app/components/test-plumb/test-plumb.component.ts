f9fcf637673ee4e553ec6b4a699adf15c3f22303
peterksts <peterk@silvertreesystems.com>
2018-07-04 17:34:53 +0300

    move select item

--- src/app/components/test-plumb/test-plumb.component.ts
+++ src/app/components/test-plumb/test-plumb.component.ts
@@ -9,6 +9,12 @@ import {jsPlumb, jsPlumbInstance} from 'jsplumb';
 })
 export class TestPlumbComponent implements OnInit {
   private jsPlumbInstance: jsPlumbInstance;
+  //
+  private mouse_start_x;
+  private mouse_start_y;
+  private move_div;
+  private main;
+  //
 
   constructor() { }
 
@@ -106,13 +112,17 @@ export class TestPlumbComponent implements OnInit {
         connectorHoverStyle: {strokeWidth: 2}
       });
     //
-    main = this;
+    this.main = this;
     this.jsPlumbInstance.repaintEverything();
+    // window resize
+    //
+    this.addSelectItems();
     //
     this.addListener();
   }
 
   private addListener(): void {
+    const main_ = this.main;
     const mainDiv = document.getElementById('diagramContainer');
     const divAll = document.getElementById('diagramContainer').
       querySelectorAll('div');
@@ -125,14 +135,14 @@ export class TestPlumbComponent implements OnInit {
           return;
         }
         //
-        move_div = this;
-        mouse_start_x = e.clientX;
-        mouse_start_y = e.clientY;
+        main_.move_div = this;
+        main_.mouse_start_x = e.clientX;
+        main_.mouse_start_y = e.clientY;
         // mouse move
-        mainDiv.addEventListener('mousemove', main.moveItem);
+        mainDiv.addEventListener('mousemove', main_.moveItem);
         // mouse up
         document.addEventListener('mouseup', function () {
-          mainDiv.removeEventListener('mousemove', main.moveItem);
+          mainDiv.removeEventListener('mousemove', main_.moveItem);
         });
       });
     }
@@ -142,36 +152,75 @@ export class TestPlumbComponent implements OnInit {
     // });
   }
 
-  private moveItem(even): void {
-    const start_position_x = parseInt(move_div.style.left.slice(0, move_div.style.left.length - 2), null);
-    const start_position_y = parseInt(move_div.style.top.slice(0, move_div.style.top.length - 2), null);
+  private moveItem = (even): void => {
+    const start_position_x = parseInt(this.move_div.style.left.slice(0, this.move_div.style.left.length - 2), null);
+    const start_position_y = parseInt(this.move_div.style.top.slice(0, this.move_div.style.top.length - 2), null);
     let new_position_x = start_position_x;
     let new_position_y = start_position_y;
     // set new x item
-    if (even.clientX > mouse_start_x) {
-      new_position_x = start_position_x + (even.clientX - mouse_start_x);
+    if (even.clientX > this.mouse_start_x) {
+      new_position_x = start_position_x + (even.clientX - this.mouse_start_x);
     }
-    if (even.clientX < mouse_start_x) {
-      new_position_x = start_position_x - (mouse_start_x - even.clientX);
+    if (even.clientX < this.mouse_start_x) {
+      new_position_x = start_position_x - (this.mouse_start_x - even.clientX);
     }
     // set new y item
-    if (even.clientY > mouse_start_y) {
-      new_position_y = start_position_y + (even.clientY - mouse_start_y);
+    if (even.clientY > this.mouse_start_y) {
+      new_position_y = start_position_y + (even.clientY - this.mouse_start_y);
     }
-    if (even.clientY < mouse_start_y) {
-      new_position_y = start_position_y - (mouse_start_y - even.clientY);
+    if (even.clientY < this.mouse_start_y) {
+      new_position_y = start_position_y - (this.mouse_start_y - even.clientY);
     }
     // set position item
-    mouse_start_x = even.clientX;
-    mouse_start_y = even.clientY;
-    move_div.style.left = new_position_x + 'px';
-    move_div.style.top = new_position_y + 'px';
-    // reset
-    main.jsPlumbInstance.repaintEverything();
+    this.mouse_start_x = even.clientX;
+    this.mouse_start_y = even.clientY;
+    this.move_div.style.left = new_position_x + 'px';
+    this.move_div.style.top = new_position_y + 'px';
+    // repaint plumb
+    this.main.jsPlumbInstance.repaintEverything();
   }
-}
 
-let mouse_start_x;
-let mouse_start_y;
-let move_div;
-let main;
+  // add item
+  private addSelectItems() {
+    const all_items_for_select = document.getElementById('diagramSelect').
+    querySelectorAll('div');
+    //
+    for (let i = 0; i < all_items_for_select.length; i++) {
+      const div = all_items_for_select.item(i);
+      // mouse down
+      div.addEventListener('mousedown', function (even) {
+        const newItem = document.createElement('div');
+        switch (this.getAttribute('name')) {
+          case 'item_select_ball':
+            newItem.setAttribute('class', 'item ball');
+            break;
+          case 'item_select_square':
+            newItem.setAttribute('class', 'item');
+            break;
+          case 'item_select_diamond':
+            newItem.setAttribute('class', 'item diamond');
+            break;
+          default:
+            return;
+        }
+        //
+        newItem.style.position = 'absolute';
+        newItem.style.left = even.clientX - 40 + 'px';
+        newItem.style.top = even.clientY - 40 + 'px';
+        newItem.id = 'new_item';
+        //
+        document.body.appendChild(newItem);
+        // mouse move
+        window.addEventListener('mousemove', function(e) {
+          newItem.style.left = e.clientX - 40 + 'px';
+          newItem.style.top = e.clientY - 40 + 'px';
+        });
+        // mouse up
+        window.addEventListener('mouseup', function () {
+          document.body.removeChild(newItem);
+        }, true);
+      });
+    }
+  }
+  //
+}
