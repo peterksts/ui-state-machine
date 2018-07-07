7a23edb101f396a9478a42f37ea33fee606f97c2
peterksts <peterk@silvertreesystems.com>
2018-07-07 08:24:53 +0300

    delete @input new task config

--- src/app/flow-editor/directives/ubix-task.directive.ts
+++ src/app/flow-editor/directives/ubix-task.directive.ts
@@ -8,19 +8,12 @@ import {Store} from '../models/store.model';
 export class UbixTaskDirective {
 
   @Input() store: Store;
-  @Input() config: Task;
 
   constructor() { }
 
   @HostListener('dragstart', ['$event'])
   onDragStart(event) {
     event.target.style.opacity = '0.6';
-
-    this.store.setStore({
-      type: 'new_ubix_task',
-      event: event,
-      data: this.config,
-    });
   }
 
   @HostListener('dragend', ['$event'])
